7e61fd3c321be1f772ebe1b76583777fd6ed393d
xuehanyu <xuehanyu@xdf.cn>
2020-11-13 14:24:48 +0800

    feat：init  react项目

---
+++ react-lesson/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; // 核心库
+import ReactDOM from 'react-dom';  // DOM渲染库
+
+ReactDOM.render(
+  <h1>HSHHSHHSH</h1>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
